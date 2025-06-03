33526d044bc6e86470bb02dd5315e3b5b9011df1
Samridhi0025 <samridhi0025@gmail.com>
2025-06-03 08:46:15 +0530

    Initial commit: Nike landing page in React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Hero from './component/Hero';
+import Navigation from './component/nav';
+const App = () => {
+  return (
+  <div>
+    <Navigation />
+    <Hero />
+    
+  </div>
+
+  );
+};
+
+export default App;
